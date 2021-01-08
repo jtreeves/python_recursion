57b9546f7c98e95aa353fd4a54fd3c2e9cbc60ef
jtreeves <jackson.t.reeves@gmail.com>
2021-01-08 16:09:04 -0500

    [recursion] Create fibonacci function

---
+++ recursion.py
@@ -0,0 +1,11 @@
+def fibonacci(n):
+    if n == 1:
+        return 1
+    elif n == 2:
+        return 2
+    elif n > 2:
+        return fibonacci(n - 1) + fibonacci(n - 2)
+
+print(fibonacci(3))
+print(fibonacci(4))
+print(fibonacci(5))
